0a57e96dd2e1872ffcc335ac07ff7ff345167fed
KidCoder-Eth <104913814+KidCoder-Eth@users.noreply.github.com>
2022-12-10 11:44:01 +0300

    0x00A

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp. with Some Requirements
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,7 @@
+# Using Puppet, install puppet-lint.
+
+package { 'puppet-lint':
+ensure   => '2.5.0',
+provider => 'gem',
+source   => 'https://rubygems.org'
+}
